ce7bb3ea6db68ed3d765ce36405a071fe7ef292d
Ben Muthalaly <benmuthalaly@gmail.com>
2026-01-04 02:51:56 -0600

    Connect to libera.chat with BearSSL.

--- main.c
+++ main.c
@@ -4,22 +4,26 @@
 #include <string.h>
 #include <sys/socket.h>
 
+#include "BearSSL/inc/bearssl_ssl.h"
+#include "trust_anchors.c"
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char **argv) {
+// connects to IRC server over IRC via TLS
+int host_connect(char *domain, struct addrinfo *info) {
   // from beej's guide to network programming
 
   // return status code (error number)
   int gai_err;
   // tell getaddrinfo what addresses you're interested in
   struct addrinfo hints;
-  // return struct for getaddrinfo
-  struct addrinfo *info;
   // addrinfo pointer for iteration through linked list
   struct addrinfo *p;
+
+  int fd;
 
   // initialize hints to 0
   memset(&hints, 0, sizeof(hints));
@@ -27,14 +31,14 @@ int main(int argc, char **argv) {
   // want the IP to be filled in
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
+  // hints.ai_flags = AI_PASSIVE;
 
-  if ((gai_err = getaddrinfo("irc.libera.chat", "6697", &hints, &info)) != 0) {
+  if ((gai_err = getaddrinfo(domain, "6697", &hints, &info)) != 0) {
     fprintf(stderr, "gai error: %s\n", gai_strerror(gai_err));
     exit(1);
   }
 
-  printf("IP Addresses for irc.libera.chat\n");
+  // printf("IP Addresses for %s\n", domain);
 
   char ipstr[INET6_ADDRSTRLEN];
   // iterate through addrinfos
@@ -55,24 +59,102 @@ int main(int argc, char **argv) {
       ipver = "IPv6";
     }
 
+    fd = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+    if (fd < 0) {
+      printf("socket err: %d\n", errno);
+      close(fd);
+      continue;
+    }
+    if (connect(fd, p->ai_addr, p->ai_addrlen) < 0) {
+      printf("connect err: %d\n", errno);
+      close(fd);
+      continue;
+    }
+
     // convert the addr into a printable numeric form and write to ipstr
+    // get the file descriptor for the socket
     inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
     printf("  %s: %s\n", ipver, ipstr);
+    break;
   }
 
-  
-  // get the file descriptor for the socket
-  int s = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
-  
+	if (p == NULL) {
+		freeaddrinfo(info);
+		fprintf(stderr, "ERROR: failed to connect\n");
+		return -1;
+	}
+	freeaddrinfo(info);
+	fprintf(stderr, "connected.\n");
+
+  return fd;
+}
+
+static int sock_read(void *ctx, unsigned char *buf, size_t len) {
+  for (;;) {
+    ssize_t rlen;
+
+    rlen = read(*(int *)ctx, buf, len);
+    if (rlen <= 0) {
+      if (rlen < 0 && errno == EINTR) {
+        continue;
+      }
+      return -1;
+    }
+    return (int)rlen;
+  }
+}
+
+static int sock_write(void *ctx, const unsigned char *buf, size_t len) {
+  for (;;) {
+    ssize_t wlen;
+    wlen = write(*(int *)ctx, buf, len);
+    if (wlen <= 0) {
+      if (wlen < 0 && errno == EINTR) {
+        continue;
+      }
+      return -1;
+    }
+    return (int)wlen;
+  }
+}
+
+int main(int argc, char **argv) {
+  br_ssl_client_context sc;
+  br_x509_minimal_context xc;
+  unsigned char iobuf[BR_SSL_BUFSIZE_BIDI];
+  br_sslio_context ioc;
+
+  char *host = "irc.libera.chat";
+
+  // NOTE: binding not needed as a client program
   // avoid "Address already in use." errors when rerunning the program
-  int yes = 1;
-  setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
-  int b_err;
-  if ((b_err = bind(s, info->ai_addr, info->ai_addrlen) != 0)) {
-    fprintf(stderr, "bind error: %d\n", errno);
-  }
+  // int yes = 1;
+  // setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+  // int b_err;
+  // if ((b_err = bind(s, info->ai_addr, info->ai_addrlen) != 0)) {
+  //   fprintf(stderr, "bind error: %d\n", errno);
+  // }
 
-  freeaddrinfo(info);
+  // output struct for getaddrinfo
+  struct addrinfo info;
+
+  int fd = host_connect(host, &info);
+
+  br_ssl_client_init_full(&sc, &xc, TAs, TAs_NUM);
+
+  br_ssl_engine_set_buffer(&sc.eng, iobuf, sizeof iobuf, 1);
+
+  br_ssl_client_reset(&sc, host, 0);
+
+  br_sslio_init(&ioc, &sc.eng, sock_read, &fd, sock_write, &fd);
+
+  // int c_err;
+  // if ((c_err = connect(fd, info.ai_addr, sizeof(struct sockaddr))) != 0) {
+  //   fprintf(stderr, "connect error: %d\n", errno);
+  // }
+
+  // close(fd);
+  // freeaddrinfo(&info);
 
   return 0;
 }
